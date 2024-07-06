aae7903854a896292c0a9e48fbd093202725c16d
Mainul Islam <maainul.islam@gmail.com>
2024-07-06 19:28:41 +0600

    chant the directory path with a period

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    console.log(`Fetching songs from: /spotify/${folder}/`);
-    let a = await fetch(`/spotify/${folder}/`);
+    console.log(`Fetching songs from: ./spotify/${folder}/`);
+    let a = await fetch(`../spotify/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -58,7 +58,7 @@ async function getSongs(folder) {
 }
 
 const playMusic = (track, pause = false) => {
-    currentSong.src = `/spotify/${currFolder}/` + track;
+    currentSong.src = `./spotify/${currFolder}/` + track;
     console.log(`Playing music: ${currentSong.src}`);
     if (!pause) {
         currentSong.play();
@@ -69,7 +69,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-    let a = await fetch(`/spotify/songs`);
+    let a = await fetch(`./spotify/songs`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -81,7 +81,7 @@ async function displayAlbums() {
 
         if (e.href.includes("/songs")) {
             let folder = e.href.split("/").slice(-2)[1];
-            let a = await fetch(`/spotify/songs/${folder}/info.json`);
+            let a = await fetch(`./spotify/songs/${folder}/info.json`);
             let response = await a.json(); 
             
             cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
@@ -92,7 +92,7 @@ async function displayAlbums() {
                             stroke-linejoin="round" />
                     </svg>
                 </div>
-                <img src="/spotify/songs/${folder}/cover.jpg" alt="">
+                <img src="./spotify/songs/${folder}/cover.jpg" alt="">
                 <h2>${response.title}</h2>
                 <p>${response.description}</p>
             </div>`;
